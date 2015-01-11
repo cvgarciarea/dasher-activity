b91a5a4bafedd023f3dbf3722bbe70dce934911a
Cristian García <cristian99garcia@gmail.com>
2015-01-10 22:59:42 -0200

    Fixing a few things

--- dasher.py
+++ dasher.py
@@ -10,14 +10,27 @@ from gi.repository import Gdk
 from gi.repository import GObject
 
 LETTERS = [chr(x) for x in range(65, 91)]
-COLORS = {'pink': (1.0, 0.6823529411764706, 0.7254901960784313),
-          'red': (1.0, 0, 0),
-          'green': (0.0, 0.7843137254901961, 0.0),
-          'sky-blue': (0.5294117647058824, 0.807843137254902, 1.0),
-          'white': (1.0, 1.0, 1.0)}
+COLORS = {'yellow': (1, 1, 0),
+          'pink': (1, 0.6823529411764706, 0.7254901960784313),
+          'pink2': (1, 0.7333333333333333, 1),
+          'red': (1, 0, 0),
+          'green': (0.0, 0.7843137254901961, 0),
+          'green2': (0.7058823529411765, 0.9333333333333333, 0.7058823529411765),
+          'sky-blue': (0.5294117647058824, 0.807843137254902, 1),
+          'sky-blue2': (0, 1, 1),
+          'white': (1, 1, 1),
+          'black': (0, 0, 0)}
 
 # Color amarillo para todas las letras
 # Teclado al estilo dasher, pero con teclas similares a las fisicas
+
+def get_random_color():
+    colors = []
+    for x in COLORS:
+        if x not in ['red', 'white', 'black', 'yellow']:
+            colors.append(COLORS[x])
+
+    return random.choice(colors)
 
 
 class LetterBox(GObject.GObject):
@@ -31,13 +44,13 @@ class LetterBox(GObject.GObject):
         GObject.GObject.__init__(self)
 
         self.letter = letter
-        self.color = COLORS['pink']
+        self.color = get_random_color()
         _width = father.width
         _height = father.height
         self.width = _width / 2.0
-        self.height = _height / 2.0
+        self.height = _height / float(len(LETTERS))
         self.x = self.width / 2.0
-        self.y = self.height / (float(len(LETTERS)) * LETTERS.index(letter) + 1)
+        self.y = self.height / (float(len(LETTERS)) * (LETTERS.index(letter) + 1))
         self.context = None
         self.selected = False
         self.boxes = []  # LetterBox children only
@@ -47,27 +60,26 @@ class LetterBox(GObject.GObject):
         self.context = context
 
     def render(self):
+        # FIXME: hacer que sea más alto si el mouse está cerca y en el mismo y
         self.width = self.father.width / 2.0
-        self.height = self.father.height / 2.0
+        self.height = self.father.height / float(len(LETTERS))
         self.x = self.father.x + self.width
-        self.y = self.father.y
+        self.y = self.father.y + self.height * LETTERS.index(self.letter)
 
         self.paint()
         self.check_selected()
-
-    def set_pos(self, x, y, center_y, width, height):
-        self.x = x
-        self.y = y
-
-        self.width = width
-        self.height = height
-        self.paint()
+        self.render_text()
 
     def paint(self):
         self.context.move_to(self.x, self.y)
         self.context.set_source_rgba(*self.color)
         self.context.rectangle(self.x, self.y, self.width, self.height)
         self.context.fill()
+
+    def render_text(self):
+        if self.height > 15:
+            self.context.move_to(self.x + 10, self.y + self.height / 2.0)
+            self.context.show_text(self.letter)
 
     def check_selected(self):
         if self.x < self._center[0]:
@@ -101,6 +113,7 @@ class InitBox(GObject.GObject):
         self.color = (0.7058823529411765, 0.9333333333333333, 0.7058823529411765)
         self.selected = False
         self.boxes = []
+        self.rendered = False
 
     def set_context(self, context):
         self.context = context
@@ -111,7 +124,7 @@ class InitBox(GObject.GObject):
             self.x = x + self.inc
             self.y = y
 
-    def move(self, x, y):
+    def render(self, x, y):
         if str(x).startswith('0.'):
             x = 0
         if str(y).startswith('0.'):
@@ -158,7 +171,7 @@ class Letters(InitBox):
         InitBox.__init__(self)
 
         self.father = father
-        self.color = COLORS['green']
+        self.color = COLORS['yellow']
         self.selected = False
 
     def render(self):
@@ -172,7 +185,7 @@ class Letters(InitBox):
 
     def check_selected(self):
         _x, _y = self._center
-        if _x > self.x and _y > self.y and _y < self.x + self.height:
+        if _x > self.x and _y > self.y and _y < self.y + self.height:
             if not self.selected:
                 self.selected = True
                 self.emit('selected')
@@ -183,10 +196,36 @@ class Letters(InitBox):
                 self.emit('unselected')
 
 
-class Character(InitBox):
+class Characters(InitBox):
 
-    def __init__(self):
+    def __init__(self, father):
         InitBox.__init__(self)
+
+        self.father = father
+        self.color = COLORS['red']
+        self.selected = False
+
+    def render(self):
+        self.width = self.father.width / 2.0
+        self.height = self.father.height / 2.0
+        self.x = self.father.x + self.width
+        self.y = self.father.y + self.height
+
+        self.paint()
+        self.check_selected()
+
+    def check_selected(self):
+        #_x, _y = self._center
+        #if _x > self.x and _y > self.y and _y < self.y + self.height:
+        #    if not self.selected:
+        #        self.selected = True
+        #        self.emit('selected')
+
+        #else:
+        #    if self.selected:
+        #        self.selected = False
+        #        self.emit('unselected')
+        pass
 
 
 class Area(Gtk.DrawingArea):
@@ -227,30 +266,35 @@ class Area(Gtk.DrawingArea):
         # 1) Letras
         # 2) Caracteres
         if isinstance(box, Letters):
-            box.selected = True
             for letter in LETTERS:
                 _box = LetterBox(letter, box)
                 _box.set_context(self.context)
-                _box.connect('selected', self.box_selected)
+                box.boxes.append(_box)
+                #_box.connect('selected', self.box_selected)
                 #_box.connect('unselected', self.box_unselected)
                 self.boxes.append(_box)
 
         elif isinstance(box, InitBox):
-            _box = Letters(self.actual_box)
-            _box.selected = True
-            _box.connect('selected', self.box_selected)
-            #_box.connect('unselected', self.box_unselected)
-            self.boxes.append(_box)
+            box1 = Letters(self.actual_box)
+            box1.connect('selected', self.box_selected)
+            box1.connect('unselected', self.box_unselected)
+            self.boxes.append(box1)
+
+            box2 = Characters(self.actual_box)
+            box2.connect('selected', self.box_selected)
+            box2.connect('unselected', self.box_unselected)
+            self.boxes.append(box2)
 
     def box_unselected(self, box):
-        if not isinstance(box, InitBox):
+        if isinstance(box, Letters):
             for _box in box.boxes:
-                self.boxes.remove(_box)
+                if _box != box:
+                    self.boxes.remove(_box)
 
             del box.boxes
             box.boxes = []
 
-        else:
+        elif isinstance(box, InitBox):
             del self.boxes
             self.boxes = []
 
@@ -260,9 +304,6 @@ class Area(Gtk.DrawingArea):
         self.height = allocation.height
         self.center = (self.width / 2.0, self.height / 2.0)
         self.context = context
-
-        self.init_box.context = context
-        self.init_box.set_center(*self.center)
 
         self.render()
 
@@ -275,7 +316,7 @@ class Area(Gtk.DrawingArea):
 
     def render(self):
         self.render_background()
-        self.move()
+        self.render_boxes()
         self.render_lines()
         self.render_line_to_mouse()
 
@@ -311,7 +352,7 @@ class Area(Gtk.DrawingArea):
         self.context.stroke()
         GObject.idle_add(self.queue_draw)
 
-    def move(self):
+    def render_boxes(self):
         x = (self.center[0] - self.mouse_pos[0]) / 10.0
         y = (self.center[1] - self.mouse_pos[1]) / 10.0
         self.init_box.context = self.context
@@ -319,29 +360,19 @@ class Area(Gtk.DrawingArea):
         if self.moving:
             self.init_box._width = self.width
             self.init_box._height = self.height
-            self.init_box._center = self.center
-            self.init_box.move(x, y)
+            self.init_box.set_center(*self.center)
+            self.init_box.render(x, y)
         else:
             self.init_box.paint()
 
         box_separation = 1
-        box_size = self.init_box.height / (len(LETTERS)) - box_separation
-        _x = self.init_box.x + self.init_box.width / 2.0
-        _y = self.init_box.y
 
         for box in self.boxes:
-            # FIXME: hacer que cada caja tenga un padre, y tomar las medidas de este padre
-            x = _x
-            y = _y
-
-            _y = y + box_size + box_separation
             box.set_context(self.context)
+            box._center = self.center
+            box.context = self.context
 
             if self.moving:
-                #box.move(x, y, self.center[1])
-                #box.set_pos(x, y, self.center[1], self.init_box.width / 2.0, box_size)
-                box._center = self.center
-                box.context = self.context
                 box.render()
             else:
                 box.paint()
